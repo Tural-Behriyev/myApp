0df37779394c2fe1cf7a1a5acdf966c6e5ec6212
Tural Behriyev <tbehriyev87@gmail.com>
2024-04-02 15:14:43 +0400

    updates

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,15 +1,52 @@
+// async awit()
+
+// const url = "http://localhost:3000/users";
+// const users = document.querySelector(".users");
+// const writeData = async () => {
+//   const { data } = await axios(url);
+//   data.forEach(({ name, surname, description, image }) => {
+//     users.innerHTML += `<div class="user border rounded-5 p-5">
+//             <img src="${image}"
+//                 alt="">
+//             <h2>${name} ${surname}</h2>
+//             <p>${description}</p>
+//             <button class="btn btn-danger">Delete user </button>
+//         </div>`;
+//   });
+// };
+// writeData();
+
+//////////////////////////////////////////////////////////////////////
+
+// fetch()
+// const url = "http://localhost:3000/users";
+// const users = document.querySelector(".users");
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.forEach(({ name, surname, description, image }) => {
+//       users.innerHTML += `<div class="user border rounded-5 p-5">
+//                <img src="${image}" alt="">
+//                <h2>${name} ${surname}</h2>
+//                <p>${description}</p>
+//                <button class="btn btn-danger">Delete user</button>
+//            </div>`;
+//     });
+//   })
+//   .catch((error) => console.error("Fetch error:", error));
+
+//////////////////////////////////////////////////////////////////////
+
+// axios()
 const url = "http://localhost:3000/users";
 const users = document.querySelector(".users");
-const writeData = async () => {
-  const { data } = await axios(url);
+axios(url).then(({ data }) => {
   data.forEach(({ name, surname, description, image }) => {
     users.innerHTML += `<div class="user border rounded-5 p-5">
-            <img src="${image}"
-                alt="">
-            <h2>${name} ${surname}</h2>
-            <p>${description}</p>
-            <button class="btn btn-danger">Delete user </button>
-        </div>`;
+                <img src="${image}" alt="">
+                <h2>${name} ${surname}</h2>
+                <p>${description}</p>
+                <button class="btn btn-danger">Delete user</button>
+            </div>`;
   });
-};
-writeData();
+});
